2e3cb4c11f37cdbb81a05fa90a06baf86f8439a7
pivnati <natanmuleta911@gmail.com>
2025-08-13 09:20:04 -1200

    modfiy the layout.tsx file

--- server/users.ts
+++ server/users.ts
@@ -2,12 +2,17 @@
 
 import {auth} from "@/lib/auth";
 export const signIn = async () =>{
-    await auth.api.signInEmail({
-        body :{
-            email:"orcdev@test.com",
-            password:"password123"
-        } 
-    })
+    try {
+        await auth.api.signInEmail({
+            body :{
+                email:"orcdev@test.com",
+                password:"password123"
+            } 
+        }) 
+    } catch (error) {
+         throw new Error('invalid email or password'); 
+    }
+    
 }
 export const signUp = async () =>{
     await auth.api.signUpEmail({
